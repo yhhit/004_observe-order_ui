b027fdb52a44374e9f29b73e0e1a40f71b79be3d
yhh <827077539@qq.com>
2022-09-08 22:50:18 +0800

    Update api.js

--- observe-order_ui/src/components/const/api.js
+++ observe-order_ui/src/components/const/api.js
@@ -1,16 +1,16 @@
-const loginUrl=`http://localhost:8080/api/sso/v1/users/login`
-const orderListUrl=`http://localhost:8080/api/backend/v1/orders/orders`
-const deleteOrdersUrl=`http://localhost:8080/api/backend/v1/orders/orders`
-const updateOrdersSuccessfulStatusUrl=`http://localhost:8080/api/backend/v1/orders/success/:status`
-const updateOrdersDoneStatusUrl=`http://localhost:8080/api/backend/v1/orders/done/:status`
-const registerUrl=`http://localhost:8080/api/sso/v1/users/register`
-const modifyProfileUrl=`http://localhost:8080/api/backend/v1/users/profile`
-const modifyPasswordUrl=`http://localhost:8080/api/backend/v1/users/profile/password`
-const userProfileUrl=`http://localhost:8080/api/backend/v1/users/profile`
-const issueOrderUrl=`http://localhost:8080/api/backend/v1/orders/order`
-const toWhomOptionsUrl=`http://localhost:8080/api/backend/v1/orders/can_to_whom`
-const dataStatisticsUrl=`http://localhost:8080/api/sso/v1/data_statistics/test`
-const deleteAccountUrl=`http://localhost:8080/api/backend/v1/users/users`
+const loginUrl=`/api/sso/v1/users/login`
+const orderListUrl=`/api/backend/v1/orders/orders`
+const deleteOrdersUrl=`/api/backend/v1/orders/orders`
+const updateOrdersSuccessfulStatusUrl=`/api/backend/v1/orders/success/:status`
+const updateOrdersDoneStatusUrl=`/api/backend/v1/orders/done/:status`
+const registerUrl=`/api/sso/v1/users/register`
+const modifyProfileUrl=`/api/backend/v1/users/profile`
+const modifyPasswordUrl=`/api/backend/v1/users/profile/password`
+const userProfileUrl=`/api/backend/v1/users/profile`
+const issueOrderUrl=`/api/backend/v1/orders/order`
+const toWhomOptionsUrl=`/api/backend/v1/orders/can_to_whom`
+const dataStatisticsUrl=`/api/sso/v1/data_statistics/test`
+const deleteAccountUrl=`/api/backend/v1/users/users`
 
 export{
     loginUrl,
